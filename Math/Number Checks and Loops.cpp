894e080c600b2091edb0a89e3d184d658fbcbf98
Prasanna Agnihotri <prasannacr7pa9@gmail.com>
2026-06-29 05:27:47 +0530

    Solved: Number Checks and Loops [Math]

---
+++ Math/Number Checks and Loops.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+// Function to check if a number is even or odd
+void checkevenodd(int n){
+    if(n%2==0){
+        cout<<"number is even"<<endl;
+    }
+    else{
+        cout<<"number is odd"<<endl;
+    }
+}
+
+// Function to check if a number is positive or negative
+void numcheck(int n){
+    if(n>0){
+        cout<<"number is +ive"<<endl;
+    }
+    else{
+        cout<<"number is -ive"<<endl;
+    }
+}
+
+// Function to print numbers from n down to 1
+void oneton(int n){
+    for(int i=n;i>=1;i--){
+        cout<<i<<" ";
+    }
+}
+
+int main(){
+    int b;
+    cout<<"enter the number to perform";
+    cin>>b;
+    checkevenodd(b);
+    numcheck(b);
+    oneton(b);
+    return 0;
+}
---
+++ Math/README.md
@@ -0,0 +1,9 @@
+# 📂 Math Problems
+
+This directory contains solutions related to Mathematical algorithms, number properties, loops, arithmetic operations, and number theory.
+
+## 📋 Problem List
+
+| Problem Name | Language | Description | Difficulty |
+| :--- | :---: | :--- | :---: |
+| [Number Checks and Loops](file:///e:/60%20Days%20Hell%20Period/Math/Number%20Checks%20and%20Loops.cpp) | C++ | Basic utility functions checking even/odd, signs, and reverse counting | Easy |
