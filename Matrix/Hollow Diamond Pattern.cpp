47c686fa90965c8337e14802654eaabe8f8527f2
Prasanna Agnihotri <prasannacr7pa9@gmail.com>
2026-07-16 14:08:58 +0530

    Add Hollow Diamond Pattern

---
+++ Matrix/Hollow Diamond Pattern.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value from n";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=(n-i);j++){
+            cout<<"  ";
+        }
+        if(i==1){
+            cout<<"* ";
+        }
+        else{
+            cout<<"* ";
+            for(int j=1;j<=(2*i-3);j++){
+            cout<<"  ";
+            }
+            cout<<"* ";
+        }
+    cout<<endl;
+    }
+    for(int i=1;i<=n-1;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"  ";
+        }
+        if(i==(n-1)){
+            cout<<"* ";
+        }
+        else{
+            cout<<"* ";
+            for(int j=1;j<=(2*(n-1)-2*i-1);j++){
+            cout<<"  ";
+            }
+            cout<<"* ";
+        }
+    cout<<endl;
+    }
+    return 0;
+}
